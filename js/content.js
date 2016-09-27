fb9f9c7eb5fed3e3f03adbc75f99f56b9d5adee1
victorVazquez <victor.zamudio@hotmail.com>
2016-09-26 22:43:13 -0500

    Adding Profile Users SMU to FB

--- js/content.js
+++ js/content.js
@@ -1,11 +1,11 @@
 /*content.js*/
 console.log('CONTENT.JS');
+var StarmeUpAddonChromeContent = StarmeUpAddonChromeContent || {};
 
-var StarmeupAddonFacebook = StarmeupAddonFacebook || {};
-StarmeupAddonFacebook.enabledLogger = true;
+StarmeUpAddonChromeContent.enabledLogger = true;
 
-StarmeupAddonFacebook.getLogger = function(name){
-  if (StarmeupAddonFacebook.enabledLogger) {
+StarmeUpAddonChromeContent.getLogger = function(name){
+  if (StarmeUpAddonChromeContent.enabledLogger) {
     return {
           debug: console.log.bind(console, '[' + name + ']: '),
           log: console.log.bind(console, '[' + name + ']: '),
@@ -24,8 +24,99 @@ StarmeupAddonFacebook.getLogger = function(name){
 
 };
 
-StarmeupAddonFacebook.render = function(){
-  var logger = StarmeupAddonFacebook.getLogger('Rendering');
+StarmeUpAddonChromeContent.findUserSMU = function(userName, element){
+  var locateUserSMU = new XMLHttpRequest();
+  var url = 'https://qa.starmeup.com/starmeup-api/v2/smart/locateusers?searchString=' + userName;
+  console.log('url: ', url);
+  var token = localStorage.getItem('TokenUserSMULogged');
+  locateUserSMU.open('GET', url, true);
+  locateUserSMU.setRequestHeader('TOKEN', token);
+  locateUserSMU.onreadystatechange = function(){
+    if (locateUserSMU.readyState == 4) {
+      var data = JSON.parse(locateUserSMU.responseText);
+      if (data.result.length > 0) {
+        console.log('dataUserFB: ', data);
+        localStorage.setItem('ProfileUsersSMU', JSON.stringify(data.result));
+        for (var i = 0; i < data.result.length; i++) {
+          if (userName === data.result[i].firstName + ' ' + data.result[i].lastName) {
+            console.log(data.result[i].firstName + ' ' + data.result[i].lastName);
+            StarmeUpAddonChromeContent.renderProfile(data.result[i], element);
+          }else{
+            // console.log(i);
+            // console.log(data.result.length);
+            // console.log(i >= (data.result.length -1) );
+            console.warn('The SMU user "' + data.result[i].firstName + ' ' + data.result[i].lastName + '" doesn\'t match with the Facebook user "' + userName + '"');
+            // if (i >= (data.result.length -1) ) {
+            //   console.log('**************************************');
+            //   console.warn('No SMU users match the Facebook user "' + userName + '"');
+            // }
+          }
+        }
+        // console.log();
+      }else{
+        console.warn('No SMU users that match the Facebook user');
+      }
+    }
+  };
+  locateUserSMU.send();
+};
+
+StarmeUpAddonChromeContent.renderProfile = function(profile, element){
+  console.log(element);
+
+  switch(element){
+    case '#pagelet_welcome_box':
+    var profileUserLogged = JSON.parse(localStorage['ProfileUserSMULogged']);
+    if ($('#StarmeUpAddonChromeContent').length === 0) {
+      $('#pagelet_welcome_box').after('<div id="StarmeUpAddonChromeContent"></div>');
+      $('#StarmeUpAddonChromeContent').hide().append('<div id="smuAddonFBHeader"><img src="https://qa.starmeup.com/assets/img/favicon.ico" width="16" height="16" /> <strong>Starmeup Profile</strong></div><div id="smuAddonFBBody"><i class="fa fa-institution" aria-hidden="true"></i> <strong>ORGANIZATION</strong><br><span>'+profileUserLogged.organizationName+'</span><br><i class="fa fa-user" aria-hidden="true"></i> <strong>POSITION</strong><br><span>'+profileUserLogged.job+'</span><br><i class="fa fa-map-marker" aria-hidden="true"></i> <strong>LOCATION</strong><br><span>'+profileUserLogged.office.name+'</span><br><i class="fa fa-briefcase" aria-hidden="true"></i> <strong>AREA</strong><br><span>'+profileUserLogged.area+'</span><br><i class="fa fa-suitcase" aria-hidden="true"></i> <strong>PROJECT</strong><br><span>'+profileUserLogged.project+'</span><br><i class="fa fa-folder-open" aria-hidden="true"></i>  <strong>ACCOUNT</strong><br><span>'+profileUserLogged.account+'</span><br></div>');
+      $('#StarmeUpAddonChromeContent').slideDown();
+    }else{
+      console.warn('The profile has been added');
+    }
+    break;
+
+    case '#fbProfileCover':
+    if ($('#StarmeUpAddonChromeContent').length === 0) {
+      $('#fbProfileCover').after('<div id="StarmeUpAddonChromeContent"></div>');
+      $('#StarmeUpAddonChromeContent').hide().append('<div id="smuAddonFBHeader"><img src="https://qa.starmeup.com/assets/img/favicon.ico" width="16" height="16" /> <strong>Starmeup Profile</strong></div><div id="smuAddonFBBody"><i class="fa fa-institution" aria-hidden="true"></i> <strong>ORGANIZATION</strong><br><span>'+profile.organizationName+'</span><br><i class="fa fa-user" aria-hidden="true"></i> <strong>POSITION</strong><br><span>'+profile.job+'</span><br><i class="fa fa-map-marker" aria-hidden="true"></i> <strong>LOCATION</strong><br><span>'+profile.organizationOfficeName+'</span><br><i class="fa fa-briefcase" aria-hidden="true"></i> <strong>AREA</strong><br><span>'+profile.area+'</span><br><i class="fa fa-suitcase" aria-hidden="true"></i> <strong>PROJECT</strong><br><span>'+profile.project+'</span><br><i class="fa fa-folder-open" aria-hidden="true"></i>  <strong>ACCOUNT</strong><br><span>'+profile.account+'</span><br></div>');
+      $('#StarmeUpAddonChromeContent').slideDown();
+      console.info('The profile has added succesfully in #fbProfileCover :)');
+    }else{
+      console.warn('The profile has been added');
+    }
+    break;
+
+    case '#pagelet_escape_hatch':
+    if ($('#StarmeUpAddonChromeContent').length === 0) {
+      $('#pagelet_escape_hatch').before('<div id="StarmeUpAddonChromeContent"></div>');
+      $('#StarmeUpAddonChromeContent').hide().append('<div id="smuAddonFBHeader"><img src="https://qa.starmeup.com/assets/img/favicon.ico" width="16" height="16" /> <strong>Starmeup Profile</strong></div><div id="smuAddonFBBody"><i class="fa fa-institution" aria-hidden="true"></i> <strong>ORGANIZATION</strong><br><span>'+profile.organizationName+'</span><br><i class="fa fa-user" aria-hidden="true"></i> <strong>POSITION</strong><br><span>'+profile.job+'</span><br><i class="fa fa-map-marker" aria-hidden="true"></i> <strong>LOCATION</strong><br><span>'+profile.organizationOfficeName+'</span><br><i class="fa fa-briefcase" aria-hidden="true"></i> <strong>AREA</strong><br><span>'+profile.area+'</span><br><i class="fa fa-suitcase" aria-hidden="true"></i> <strong>PROJECT</strong><br><span>'+profile.project+'</span><br><i class="fa fa-folder-open" aria-hidden="true"></i>  <strong>ACCOUNT</strong><br><span>'+profile.account+'</span><br></div>');
+      $('#StarmeUpAddonChromeContent').slideDown();
+      console.info('The profile has added succesfully in #pagelet_escape_hatch :)');
+    }else{
+      console.warn('The profile has been added');
+    }
+    break;
+
+    case '#pagelet_pymk_timeline':
+    if ($('#StarmeUpAddonChromeContent').length === 0) {
+      $('#pagelet_pymk_timeline').before('<div id="StarmeUpAddonChromeContent"></div>');
+      $('#StarmeUpAddonChromeContent').hide().append('<div id="smuAddonFBHeader"><img src="https://qa.starmeup.com/assets/img/favicon.ico" width="16" height="16" /> <strong>Starmeup Profile</strong></div><div id="smuAddonFBBody"><i class="fa fa-institution" aria-hidden="true"></i> <strong>ORGANIZATION</strong><br><span>'+profile.organizationName+'</span><br><i class="fa fa-user" aria-hidden="true"></i> <strong>POSITION</strong><br><span>'+profile.job+'</span><br><i class="fa fa-map-marker" aria-hidden="true"></i> <strong>LOCATION</strong><br><span>'+profile.organizationOfficeName+'</span><br><i class="fa fa-briefcase" aria-hidden="true"></i> <strong>AREA</strong><br><span>'+profile.area+'</span><br><i class="fa fa-suitcase" aria-hidden="true"></i> <strong>PROJECT</strong><br><span>'+profile.project+'</span><br><i class="fa fa-folder-open" aria-hidden="true"></i>  <strong>ACCOUNT</strong><br><span>'+profile.account+'</span><br></div>');
+      $('#StarmeUpAddonChromeContent').slideDown();
+      console.info('The profile has added succesfully in #pagelet_escape_hatch :)');
+    } else{
+      console.warn('The profile has been added');
+    }
+    break;
+  }
+
+
+
+};
+
+
+StarmeUpAddonChromeContent.render = function(){
+  var logger = StarmeUpAddonChromeContent.getLogger('Rendering');
   var pagelet_welcome_box = $('#pagelet_welcome_box');
   var profile = JSON.parse(localStorage['profileLS']);
   /**/
@@ -43,53 +134,154 @@ StarmeupAddonFacebook.render = function(){
     return false;
   }else{
     // logger.log('pagelet_welcome_box: ', pagelet_welcome_box);
-    if ($('#starmeUpAddonFacebook').length === 0) {
-      $('#pagelet_welcome_box').after('<div id="starmeUpAddonFacebook"></div>');
-      $('#starmeUpAddonFacebook').hide().append('<div id="smuAddonFBHeader"><img src="https://qa.starmeup.com/assets/img/favicon.ico" width="16" height="16" /> <strong>Starmeup Profile</strong></div><div id="smuAddonFBBody"><i class="fa fa-institution" aria-hidden="true"></i> <strong>ORGANIZATION</strong><br><span>'+profile.organizationName+'</span><br><i class="fa fa-user" aria-hidden="true"></i> <strong>POSITION</strong><br><span>'+profile.job+'</span><br><i class="fa fa-map-marker" aria-hidden="true"></i> <strong>LOCATION</strong><br><span>'+profile.office.name+'</span><br><i class="fa fa-briefcase" aria-hidden="true"></i> <strong>AREA</strong><br><span>'+profile.area+'</span><br><i class="fa fa-suitcase" aria-hidden="true"></i> <strong>PROJECT</strong><br><span>'+profile.project+'</span><br><i class="fa fa-folder-open" aria-hidden="true"></i>  <strong>ACCOUNT</strong><br><span>'+profile.account+'</span><br></div>');
-      $('#starmeUpAddonFacebook').slideDown();
-      logger.info('StarmeupAddonFacebook added :)');
+    if ($('#StarmeUpAddonChromeContent').length === 0) {
+      $('#pagelet_welcome_box').after('<div id="StarmeUpAddonChromeContent"></div>');
+      $('#StarmeUpAddonChromeContent').hide().append('<div id="smuAddonFBHeader"><img src="https://qa.starmeup.com/assets/img/favicon.ico" width="16" height="16" /> <strong>Starmeup Profile</strong></div><div id="smuAddonFBBody"><i class="fa fa-institution" aria-hidden="true"></i> <strong>ORGANIZATION</strong><br><span>'+profile.organizationName+'</span><br><i class="fa fa-user" aria-hidden="true"></i> <strong>POSITION</strong><br><span>'+profile.job+'</span><br><i class="fa fa-map-marker" aria-hidden="true"></i> <strong>LOCATION</strong><br><span>'+profile.office.name+'</span><br><i class="fa fa-briefcase" aria-hidden="true"></i> <strong>AREA</strong><br><span>'+profile.area+'</span><br><i class="fa fa-suitcase" aria-hidden="true"></i> <strong>PROJECT</strong><br><span>'+profile.project+'</span><br><i class="fa fa-folder-open" aria-hidden="true"></i>  <strong>ACCOUNT</strong><br><span>'+profile.account+'</span><br></div>');
+      $('#StarmeUpAddonChromeContent').slideDown();
+      logger.info('StarmeUpAddonChromeContent added :)');
     }else{
-      logger.warn('StarmeupAddonFacebook already added :(|)');
+      logger.warn('StarmeUpAddonChromeContent already added :(|)');
     }
     return true;
   }
 
 };
 
-StarmeupAddonFacebook.remove = function () {
-  var logger = StarmeupAddonFacebook.getLogger('Remove Profile');
-  $('#starmeUpAddonFacebook').slideUp(function(){
-    $('#starmeUpAddonFacebook').remove();
+StarmeUpAddonChromeContent.remove = function () {
+  var logger = StarmeUpAddonChromeContent.getLogger('Remove Profile');
+  $('#StarmeUpAddonChromeContent').slideUp(function(){
+    $('#StarmeUpAddonChromeContent').remove();
     localStorage.removeItem('profileLS');
     localStorage.removeItem('profile_loaded');
-    logger.info('StarmeupAddonFacebook removed');
+    logger.info('StarmeUpAddonChromeContent removed');
   });
 };
 
+StarmeUpAddonChromeContent.renderElements = function(elements){
+  var profileUserLogged = JSON.parse(localStorage['ProfileUserSMULogged']);
+  var profile = profileUserLogged;
+
+  console.log(elements);
+  var numElements = elements.length;
+  for (var i = 0; i < numElements; i++) {
+    // $(elements[i]).hide();
+    // console.log(elements[i]);
+    switch(elements[i]){
+      case '#pagelet_welcome_box':
+      if ($('#StarmeUpAddonChromeContent').length === 0) {
+        $('#pagelet_welcome_box').after('<div id="StarmeUpAddonChromeContent"></div>');
+        $('#StarmeUpAddonChromeContent').hide().append('<div id="smuAddonFBHeader"><img src="https://qa.starmeup.com/assets/img/favicon.ico" width="16" height="16" /> <strong>Starmeup Profile</strong></div><div id="smuAddonFBBody"><i class="fa fa-institution" aria-hidden="true"></i> <strong>ORGANIZATION</strong><br><span>'+profileUserLogged.organizationName+'</span><br><i class="fa fa-user" aria-hidden="true"></i> <strong>POSITION</strong><br><span>'+profileUserLogged.job+'</span><br><i class="fa fa-map-marker" aria-hidden="true"></i> <strong>LOCATION</strong><br><span>'+profileUserLogged.office.name+'</span><br><i class="fa fa-briefcase" aria-hidden="true"></i> <strong>AREA</strong><br><span>'+profileUserLogged.area+'</span><br><i class="fa fa-suitcase" aria-hidden="true"></i> <strong>PROJECT</strong><br><span>'+profileUserLogged.project+'</span><br><i class="fa fa-folder-open" aria-hidden="true"></i>  <strong>ACCOUNT</strong><br><span>'+profileUserLogged.account+'</span><br></div>');
+        $('#StarmeUpAddonChromeContent').slideDown();
+      }
+      break;
+
+      case '#fbProfileCover':
+      $('#fbProfileCover').after('<div id="StarmeUpAddonChromeContent"></div>');
+      $('#StarmeUpAddonChromeContent').hide().append('<div id="smuAddonFBHeader"><img src="https://qa.starmeup.com/assets/img/favicon.ico" width="16" height="16" /> <strong>Starmeup Profile</strong></div><div id="smuAddonFBBody"><i class="fa fa-institution" aria-hidden="true"></i> <strong>ORGANIZATION</strong><br><span>'+profile.organizationName+'</span><br><i class="fa fa-user" aria-hidden="true"></i> <strong>POSITION</strong><br><span>'+profile.job+'</span><br><i class="fa fa-map-marker" aria-hidden="true"></i> <strong>LOCATION</strong><br><span>'+profile.office.name+'</span><br><i class="fa fa-briefcase" aria-hidden="true"></i> <strong>AREA</strong><br><span>'+profile.area+'</span><br><i class="fa fa-suitcase" aria-hidden="true"></i> <strong>PROJECT</strong><br><span>'+profile.project+'</span><br><i class="fa fa-folder-open" aria-hidden="true"></i>  <strong>ACCOUNT</strong><br><span>'+profile.account+'</span><br></div>');
+      $('#StarmeUpAddonChromeContent').slideDown();
+      break;
+    }
+  }
+
+
+
+};
+
+StarmeUpAddonChromeContent.lookUpRenderElements = function(elements){
+  var numElements = elements.length;
+
+  var elements_str = elements.splice(',').join(', ');
+  var elements_found = $('body').find($(elements_str));
+  var elements_arr = elements_str.split(', ');
+
+  // console.info('elements_found: ', elements_found);
+  // console.log('Elements Arr: ', elements_arr);
+  // console.log('Elements found: ', elements_found);
+  // console.log('typeof elements_found: ', typeof elements_found);
+  // console.log('elements_found.length: ', elements_found.length);
+
+  if (elements_found.length > 0) {
+    // var numElements = elements_found.length;
+    // console.log('***********************');
+    // console.log('elements.length: ', elements.length);
+    for (var i = 0; i < elements_found.length; i++) {
+      // console.warn(elements_found[i].id);
+      // $(elements[i]).hide();
+      switch(elements_found[i].id){
+        case 'pagelet_welcome_box':
+        var userName = $('#pageTitle').text();
+        var element = '#pagelet_welcome_box';
+        // alert('hi' + userName);
+        StarmeUpAddonChromeContent.findUserSMU(userName, element);
+
+        break;
+
+        // case '#fbProfileCover':
+        case 'pagelet_escape_hatch':
+        var userName = $('#pageTitle').text();
+        var element = '#pagelet_escape_hatch';
+        // alert('hi' + userName);
+        StarmeUpAddonChromeContent.findUserSMU(userName, element);
+        break;
+
+        case 'pagelet_pymk_timeline':
+        var userName = $('#pageTitle').text();
+        var element = '#pagelet_pymk_timeline';
+        // alert('hi' + userName);
+        StarmeUpAddonChromeContent.findUserSMU(userName, element);
+        break;
+      }
+    }
+    // StarmeUpAddonChromeContent.renderElements(elements_found);
+  }else{
+    console.warn('Cant find elements to render');
+  }
+
+
+};
+
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  // console.log('onMessage');
-  // console.log('message: ', message);
+  console.log('onMessage');
+  console.log('message: ', message);
 
   switch(message.type){
     case 'profile':
       // console.log('message.profile');
       var profile_str = JSON.stringify(message.profile);
       localStorage.setItem('profile_loaded', true);
-      localStorage.setItem('profileLS', profile_str);
-      StarmeupAddonFacebook.render();
+      localStorage.setItem('profileLS', profile_str);UserLogged
+      StarmeUpAddonChromeContent.render();
     break;
 
     case 'load':
       // console.log('message.page_loaded');
       // console.log("localStorage['profileLS']: ", localStorage['profileLS']);
       if ('profileLS' in localStorage) {
-        StarmeupAddonFacebook.render();
+        StarmeUpAddonChromeContent.render();
+      }
+      if ('UserSMUIsLogged' in localStorage) {
+        StarmeUpAddonChromeContent.lookUpRenderElements(['#pagelet_welcome_box', '#pagelet_pymk_timeline', '#pagelet_escape_hatch']);
       }
     break;
 
     case 'remove':
       // console.log('message.remove');
-      StarmeupAddonFacebook.remove();
+      StarmeUpAddonChromeContent.remove();
+    break;
+
+    // from authenticateUserSMU
+    case 'userLogged':
+      // console.log(message.profileUserSMULogged);
+      // console.log(message.token);
+      localStorage.setItem('UserSMUIsLogged', 'true');
+      localStorage.setItem('TokenUserSMULogged', message.token);
+      localStorage.setItem('ProfileUserSMULogged', JSON.stringify(message.profileUserSMULogged));
+      // StarmeUpAddonChromeContent.renderElements([
+      //   {'element':'#pagelet_welcome_box', 'type':'profileUser'},
+      //   {'element': '#fbProfileCover', 'type':'profileUserFB'},
+      //   {'element': '#pagelet_escape_hatch', 'type':'profileUserFB'}
+      // ]);
+      StarmeUpAddonChromeContent.lookUpRenderElements(['#pagelet_welcome_box', '#pagelet_pymk_timeline', '#pagelet_escape_hatch']);
     break;
 
   }
--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,34 @@
 window.onload = function(){
   console.log('POPUP.JS');
+  var StarmeUpAddonChromePopUp = StarmeUpAddonChromePopUp || {};
+
+  StarmeUpAddonChromePopUp.authenticateUserSMU = function(){
+    // console.log(StarmeUpAddonChromePopUp.username);
+    // console.log(StarmeUpAddonChromePopUp.password);
+    var requestUserSMUData = new XMLHttpRequest();
+    var url = 'https://qa.starmeup.com/starmeup-api/v2/sec/authenticateuser/?email=' + StarmeUpAddonChromePopUp.username + '&password=' + StarmeUpAddonChromePopUp.password;
+    requestUserSMUData.open('POST', url, true);
+    requestUserSMUData.onreadystatechange = function(){
+      if (requestUserSMUData.readyState == 4) {
+        var data = JSON.parse(requestUserSMUData.responseText);
+        // console.log('data: ', data);
+        // console.log('data.errorCode: ', data.errorCode);
+        if (data.errorCode == 100) { // fail validation
+          $('#formStarmeUp .error').addClass('show');
+        }else{
+          $('#formStarmeUp .error').removeClass('show');
+          var profileUserSMULogged = data.result;
+          var tokenSMULogged = data.token;
+          chrome.tabs.query({'active': true}, function(Tabs){
+            // console.log(Tabs);
+            // chrome.tabs.sendMessage(Tabs[1].id, {'type': 'profile', 'profile': profile});
+            chrome.tabs.sendMessage(Tabs[1].id, {'type': 'userLogged', 'token': tokenSMULogged, 'profileUserSMULogged': profileUserSMULogged});
+          });
+        }
+      }
+    };
+    requestUserSMUData.send();
+  };
 
   $('#logOut').on('click', function(e){
     chrome.tabs.getSelected(null, function(tab){
@@ -8,34 +37,13 @@ window.onload = function(){
   });
 
   $('#logIn').on('click', function(e){
-    _starmeup_userName = $('#username').val();
-    _starmeup_passWord = $('#password').val();
-
-    var xhr = new XMLHttpRequest();
-    // var url = 'https://qa.starmeup.com/starmeup-api/v2/stellar/leaderboard?organizationValue=0&organizationOffice=0&cityId=0&countryId=0&o=SMU_WEB&v=2&size=10&fromStringDate=0&toStringDate=' + date;
-    // var url = 'https://starmeup.com/starmeup-api/v2/sec/authenticateuser/?email='+ _starmeup_userName + '&password=' + _starmeup_passWord;
-    var url = 'https://qa.starmeup.com/starmeup-api/v2/sec/authenticateuser/?email='+ _starmeup_userName + '&password=' + _starmeup_passWord;
-    // var token = 'eyJleHBpcmVUaW1lIjoxNDgyMDkzMDE3NzQ0LCJ1c2VySWQiOjMzNjM4fQ.dE6_3PMDD706p374TUE07Y2BHyCccPdg_hqbTQJwBMhXjMuGgBX6lM7iR__NoREGCpGv67o06gSrlCIPR6OwxxfptbGPAaiMhHZyv2pd5nsEBgfMxe-A_I_7YRmfw1X7ihMb2GduwNtmcLPaSrHK56RGRux2r7S1aLIzacFiCRk1SX_XKjtppzWKprDnoRhGoiPTNF5vrBusxAFup3jjwH3plWwZAL1SHLQh9U7oOvfPQbQ0V_LKBJDBiPba82lahvRAnGf_eHsx4VzQVHPh9Ny-gkh7kH-xsqqYHE34ObgPuhasFOSmOw6sLjsvuwPTO1Zyp0qkbbPvbN4B7-qxDg';
-    xhr.open('POST', url, true);
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState == 4) {
-        // console.log('xhr.responseText: ', xhr.responseText);
-        var data = JSON.parse(xhr.responseText);
-        // console.log('data.errorCode: ', data.errorCode);
-        if (data.errorCode == 100) { // fail validation
-          $('#formStarmeUp .error').addClass('show');
-        }else{
-          $('#formStarmeUp .error').removeClass('show');
-          var profile = data.result;
-
-          chrome.tabs.query({'active': true}, function(Tabs){
-            // console.log(Tabs);
-            chrome.tabs.sendMessage(Tabs[1].id, {'type': 'profile', 'profile': profile});
-          });
-        }
-      }
+    StarmeUpAddonChromePopUp.username = $('#username').val();
+    StarmeUpAddonChromePopUp.password = $('#password').val();
+    // If user is already logged
+    if (localStorage.getItem('userSMULogged') === null) {
+      StarmeUpAddonChromePopUp.authenticateUserSMU();
+    }else{
+      // Do something
     }
-    xhr.send();
-
   });
 };
